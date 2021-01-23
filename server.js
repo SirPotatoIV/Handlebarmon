167a5a175c3055e56fe618bce5f12f59ffa27525
Jake O'Toole <otoolej33@gmail.com>
2021-01-23 00:04:23 -0600

    Started initializing handlebars.

--- server.js
+++ server.js
@@ -1,31 +1,27 @@
-// *****************************************************************************
-// Server.js - This file is the initial starting point for the Node/Express server.
-//
-// ******************************************************************************
-// *** Dependencies
-// =============================================================
-var express = require('express');
+let express = require('express');
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080;
+let app = express();
+let PORT = process.env.PORT || 8080;
 
-// Requiring our models for syncing
-var db = require('./models');
+// Import all models
+let db = require('./models');
 
-// Sets up the Express app to handle data parsing
+// Add middleware to Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Initialize handlebars
+let exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // Static directory
 app.use(express.static('public'));
 
 // Routes
 // =============================================================
-// require("./routes/html-routes.js")(app);
-// require("./routes/author-api-routes.js")(app);
-// require("./routes/post-api-routes.js")(app);
+// let routes = require("./controllers/trainerController.js")
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
