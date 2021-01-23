9c2d2ae259048bf3379ee9105603fea8b4fcce0a
Jake O'Toole <otoolej33@gmail.com>
2021-01-22 23:32:28 -0600

    Trainer seed finally working.

---
+++ seeders/20210123052455-demo-user.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert('Trainers', [
+      {
+        name: 'Ash Ketchum',
+        badgeCount: 8,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Jake O'Toole",
+        badgeCount: 0,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Trainers', null, {});
+  },
+};
